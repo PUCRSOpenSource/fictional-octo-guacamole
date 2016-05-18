59c6a11811be4b8717a55ee561777ff8ab73c0e9
Matthias Nunes <matthiasnunes@gmail.com>
2016-05-18 20:15:57 -0300

    Skelleton ready

--- src/parallel.c
+++ src/parallel.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
-	
+	int my_rank;
+	int proc_n;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
+
+	//Do what we need to do
+
+	MPI_Finalize();
+
 	return 0;
 }

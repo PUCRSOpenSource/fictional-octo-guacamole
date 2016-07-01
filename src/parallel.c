c26d9417d16afea08c9a7cb45bcfd0cd2950e7bd
Matthias Nunes <matthiasnunes@gmail.com>
2016-07-01 20:52:18 -0300

    Fixes style again

--- src/parallel.c
+++ src/parallel.c
@@ -42,12 +42,12 @@ interleaving (int vetor[], int tam)
   i2 = tam / 2;
 
   for (i_aux = 0; i_aux < tam; i_aux++)
-  {
-    if (((vetor[i1] <= vetor[i2]) && (i1 < (tam / 2))) || (i2 == tam))
-      vetor_auxiliar[i_aux] = vetor[i1++];
-    else
-      vetor_auxiliar[i_aux] = vetor[i2++];
-  }
+    {
+      if (((vetor[i1] <= vetor[i2]) && (i1 < (tam / 2))) || (i2 == tam))
+        vetor_auxiliar[i_aux] = vetor[i1++];
+      else
+        vetor_auxiliar[i_aux] = vetor[i2++];
+    }
 
   return vetor_auxiliar;
 }
